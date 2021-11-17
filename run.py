2f102aa06bf45e7d793b4f8b2930c0c529e66afa
bobbydeane <robertdeane1992@gmail.com>
2021-11-17 21:56:51 +0000

    Check if I can return data from the Sheet and installed num.py

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('CREDS.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('word_meaning_examples')
+
+def word_meaning():
+    word_meaning_examples = SHEET.worksheet('word_meaning_examples')
+    data = word_meaning_examples.get_all_values()[1]
+    print(data)
+
+word_meaning()
+
