5ae7aecb249784cb6af093e9ff15b368d1dd3921
bobbydeane <robertdeane1992@gmail.com>
2021-11-25 21:51:48 +0000

    Cleaned up some Spacing errors

--- run.py
+++ run.py
@@ -1,13 +1,16 @@
 import gspread
 import random
 from google.oauth2.service_account import Credentials
-from random import randrange # this import will be used when getting a random row from the Google sheet
-from termcolor import colored # this import will be used to colourize the terminal
-from datetime import datetime # this import will be used to display the date for the word of the day mode
+from random import randrange
+# this import will be used when getting a random row from the Google sheet
+from termcolor import colored
+# this import will be used to colourize the terminal
+from datetime import datetime
+# this import will be used to display the date for the word of the day mode
 from pyfiglet import Figlet
 import os
 
-f = Figlet(font = 'standard')
+f = Figlet(font='standard')  # Figlet font variable
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -19,30 +22,30 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('word_meaning_examples')
 
-word_meaning_examples = SHEET.worksheet('word_meaning_examples') # The name of the collective dictionary dataset
-word_column = word_meaning_examples.col_values(1) # The word column variable is a list of all the words in our dicitonary
+word_meaning_examples = SHEET.worksheet('word_meaning_examples')
+# The name of the collective dictionary dataset
+word_column = word_meaning_examples.col_values(1)
+# The word column variable is a list of all the words in our dicitonary
 
-# word_def_sheet = word_meaning_examples.col_values()
-
-row_count = len(word_meaning_examples.col_values(1))  # counts all rows with data entries in col1
+row_count = len(word_meaning_examples.col_values(1))    # counts all rows with data entries in col1
 row_ref_start = row_count + 1  # accounts for Sheets rows starting at 1
 word = "word"
 meaning = "meaning"
 word_as_list = "clue"
 
 
-
 def cls():
     """
-    This function clears the terminal.
+    This function clears the terminal.  Taken from stack overflow.
     """
-    os.system('cls' if os.name=='nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def main_menu():
     """
     This displays the main menu / opening terminal.
-    The user must choose a menu item by typing the corresponding number from the menu list.;
+    The user must choose a menu item by typing the
+    corresponding number from the menu list.;
     The users choice will then be passed to the validate usser choice function
 
     """
@@ -73,33 +76,25 @@ def validate_menu_choice(menu_choice):
 
     number = menu_choice
     if number == 1:
-        print(f"You have selected menu item {number}")
-        #This function will launch the word game if the user selects play game from the menu.
+        print(f"You have selected menu item {number}")  # This function will launch the word game from menu
         word_game(row_ref_start)
 
-    elif number == 2:
-        # Definition game
+    elif number == 2:  # Definition game
         print(f"You have selected menu item {number}")
         cls()
         definition_game(row_ref_start)
 
-    elif number == 3:
-        # word of the day
+    elif number == 3:  # word of the day
         print(f"You have selected menu item {number}")
         cls()
         word_of_the_day()
 
-    elif number == 4:
-        # dictionary search
+    elif number == 4:  # dictionary search
         cls()
         print(f"You have selected menu item {number}")
         dictionary_search_loop()
         
-
-
-    elif number == 5:
-
-        # how to play
+    elif number == 5:  # how to play
         print(f"You have selected menu item {number}")
         cls()
         print("How to play the Daily Dictionary word games.\n")
@@ -119,13 +114,10 @@ def validate_menu_choice(menu_choice):
         if a:
             cls()
             main_menu()
-        
-        if return_to_menu == "y":
+        if a == "y":
             cls()
             main_menu()
-        
         else: print("Please enter 'y' to return to the main menu.")
-        
 
     else: print(f"{number} is not valid"), main_menu()
 
@@ -135,22 +127,20 @@ def word_game(row_ref_start):
     """
     The Word game function displays the Game text, shuffled word, meaning and the user input.
     The user input will = user answer. This user answer will then be passed to the validate user answer function.
-    
     """
-    
     while True:
-        random_row = word_meaning_examples.row_values(randrange(2, row_ref_start)) # selects a row form the sheet at random
+        random_row = word_meaning_examples.row_values(randrange(2, row_ref_start))  # selects a row form the sheet at random
 
         global word
-        word = random_row[0] # assigns the value of column 0 (words column) in the sheet to the word variable
+        word = random_row[0]  # assigns the value of column 0 (words column) in the sheet to the word variable
         global meaning
-        meaning = random_row[1] # assigns the value of column 1 (meanings column) in the sheet to the meaning variable
+        meaning = random_row[1]  # assigns the value of column 1 (meanings column) in the sheet to the meaning variable
         global word_as_list
-        word_as_list = list(word) # converts out word to a list so it can be shiffled and displayed as a clue
-        random.shuffle(word_as_list) # shuffles the word/clue
+        word_as_list = list(word)  # converts out word to a list so it can be shiffled and displayed as a clue
+        random.shuffle(word_as_list)  # shuffles the word/clue
 
         out_str = " "
-        clue = out_str.join(word_as_list) # puts the shuffled word back together again as a clue for the user. 
+        clue = out_str.join(word_as_list)  # puts the shuffled word back together again as a clue for the user.
 
         word_as_list = list(word)  # Converts our word into a list so it can be shuffled.
         random.shuffle(word_as_list)  # Shuffles our word so the user has a clue of what word they are searching for.
@@ -167,36 +157,31 @@ def word_game(row_ref_start):
             break
         return main_menu()
 
+
 def definition_game(row_ref_start):
     cls()
     """
     The Definition game function will create a word/definition combo as well as an additonal 2 words.
     The game will display the defintion and 3 words and the user will have to choose which word
     matches the definiton.
-    
     """
 
-    random_row = word_meaning_examples.row_values(randrange(2, row_ref_start)) # the random_row x3 variables are the 3 random words
+    random_row = word_meaning_examples.row_values(randrange(2, row_ref_start)) 
+    # the random_row x3 variables are the 3 random words
     random_row2 = word_meaning_examples.row_values(randrange(2, row_ref_start))
-    random_row3 = word_meaning_examples.row_values(randrange(2, row_ref_start))
-        
+    random_row3 = word_meaning_examples.row_values(randrange(2, row_ref_start))    
     global word
     word = random_row[0]
     global meaning
     meaning = random_row[1]
     word2 = random_row2[0]
-    word3 = random_row3[0]
-        
+    word3 = random_row3[0]    
     definition_game_words = [word, word2, word3]
     print(colored(f.renderText("Definition's Game"), 'blue'))
     print(f"Definition: {meaning}.\n")
     print(f"Please choose the corect word to match the Definition: {definition_game_words}\n")
     user_def_answer = input("Enter your answer here:\n")
     validate_user__definition_answer(user_def_answer)
-
-    
-
-
 
 def validate_user_answer(user_answer):
     """
@@ -205,28 +190,24 @@ def validate_user_answer(user_answer):
     If the answer does not match the word, an error will be shown
     """
 
-    lowercase_answer = word.lower() # this will account for the users answer being all lowercase
+    lowercase_answer = word.lower()  # this will account for the users answer being all lowercase
     validate_answer = user_answer
     if validate_answer == word:
         print(f"Correct! {word}: {meaning}")
-        play_again = input("Would you like to play again? y/n:")
-        
-        
+        play_again = input("Would you like to play again? y/n:")      
         if play_again == "y":
             cls()
             word_game(row_ref_start)
         elif play_again == "n":
             cls()
             main_menu()
-        else: print("Please enter y/n.")
-        
+        else: print("Please enter y/n.") 
         #this will reset the game
 
     elif validate_answer == lowercase_answer:
         #this will return a correct answer if the user doesn't returns a lowercase correct answer
         print(f"Correct! {word}: {meaning}")
         play_again = input("Would you like to play again? y/n:")
-        
         if play_again == "y":
             cls()
             word_game(row_ref_start)
@@ -244,11 +225,8 @@ def validate_user_answer(user_answer):
         elif play_again == "n":
             cls()
             main_menu()
-        else: print(f"{play_again} is not a valid input. Please enter y/n.")
-        
-        
+        else: print(f"{play_again} is not a valid input. Please enter y/n.")       
     else: user_answer = input("That isn't the corrent word. Please try again.\n Enter your answer here:\n")
-
 
 
 def validate_user__definition_answer(user_def_answer):
@@ -259,8 +237,6 @@ def validate_user__definition_answer(user_def_answer):
     """
     lowercase_def_answer = word.lower()
     validate_def_answer = user_def_answer
-    
-
     if validate_def_answer == word:
         print(f"Correct! {word}: {meaning}")
         play_again = input("Would you like to play again? y/n:")
@@ -276,7 +252,6 @@ def validate_user__definition_answer(user_def_answer):
         #this will return a correct answer if the user doesn't returns a lowercase correct answer
         print(f"Correct! {word}: {meaning}")
         play_again = input("Would you like to play again? y/n:")
-        
         if play_again == "y":
             cls()
             definition_game(row_ref_start)
@@ -295,9 +270,6 @@ def validate_user__definition_answer(user_def_answer):
             cls()
             main_menu()
         else: print(f"{play_again} is not a valid input. Please enter y/n."), definition_game(row_ref_start)
-        
-        
-        
     else: user_def_answer = input("That isn't the corrent word. Please try again.\n Enter your answer here:\n")
 
 
@@ -324,8 +296,7 @@ def dictionary_search_loop():
             cls()
             main_menu()
 
-        elif user_search_capitalize != "menu": 
-            
+        elif user_search_capitalize != "menu":
             if user_search_capitalize in word_column:
                 print(SHEET.worksheet('word_meaning_examples').row_values(SHEET.worksheet('word_meaning_examples').find(f"{user_search_capitalize}").row))
                 search_again = input("Would you like to search again? y/n:")
@@ -337,7 +308,6 @@ def dictionary_search_loop():
                     main_menu()
                 else: print("invalid input"), dictionary_search_loop()
             else: print("Sorry, we unfortunately we don't have that word in our Dictionary, please search for another word"), dictionary_search_loop()
-    
         else: print("Sorry, we unfortunately we don't have that word in our Dictionary, please search for another word.\n"), dictionary_search_loop()
 
     except ValueError: print("Apologies! That word isn't in our Dictionary collection, please try again."), dictionary_search_loop()
@@ -365,8 +335,4 @@ def main():
     definition_game(row_ref_start)
     word_of_the_day()
     dictionary_search_loop()
-    
 main()
-
-
-
